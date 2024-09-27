38c5956583eca677cce973d876ffbb98baad26ef
Cyril Vincent <contact@cyrilvincent.com>
2024-09-27 15:33:15 +0200

    demo

---
+++ poc/barchar2demo/urban.py
@@ -0,0 +1,37 @@
+import bar_chart_race as bcr
+print(bcr.__version__)
+df = bcr.load_dataset('urban_pop')
+print(df)
+bcr.bar_chart_race(
+        df=df,
+        filename='urban.gif',
+        orientation='h',
+        sort='desc',
+        n_bars=8,
+        fixed_order=False,
+        fixed_max=False,
+        steps_per_period=20,
+        period_length=500,
+        # end_period_pause=0,
+        interpolate_period=True,
+        period_label={'x': .98, 'y': .3, 'ha': 'right', 'va': 'center'},
+        period_template='%Y',
+        period_summary_func=lambda v, r: {'x': .98, 'y': .2,
+                                          's': f'Population: {v.sum() / 1000000: .0f}M',
+                                          'ha': 'right', 'size': 11},
+        # perpendicular_bar_func='median',
+        colors='dark12',
+        title='Urban population by Country',
+        bar_size=.95,
+        bar_textposition='inside',
+        bar_texttemplate='{x:.0f}',
+        bar_label_font=7,
+        tick_label_font=7,
+        tick_template='{x:.0f}',
+        shared_fontdict=None,
+        scale='linear',
+        fig=None,
+        writer=None,
+        bar_kwargs={'alpha': .7},
+        fig_kwargs={'figsize': (6, 3.5), 'dpi': 144},
+        filter_column_colors=False)
