06869f3fd5106e0884ebe9ce63612b036459d33a
Cyril Vincent <contact@cyrilvincent.com>
2025-02-28 12:29:19 +0100

    test

---
+++ config.py
@@ -0,0 +1,8 @@
+name = "ISCRI"
+version = "1.0.0.Beta"
+copyright = "(c) Skema 2024"
+connection_string = "postgresql://postgres:sa@localhost:5432/iscri"
+download_path = "d:/iscri/download"
+iscri_threshold = 1e-2
+nb_row_commit = 1000000
+
