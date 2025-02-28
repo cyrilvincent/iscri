1b0c90a6f46164d3736821263895387da26cf899
Cyril Vincent <contact@cyrilvincent.com>
2025-02-28 13:25:40 +0100

    force_download

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ name = "ISCRI"
 version = "1.0.0.RC"
 copyright = "(c) Skema 2024"
 connection_string = "postgresql://postgres:sa@localhost:5432/iscri"
-download_path = "d:/iscri/download2" if os.name == "nt" else "~/download"
+download_path = "d:/iscri/download2" if os.name == "nt" else "/home/azureuseriscri/download"
 iscri_threshold = 1e-2
 nb_row_commit = 1000000
 
