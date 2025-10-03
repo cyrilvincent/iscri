9a06b79391c66661ef3dc933625668f06d894d09
Cyril Vincent <contact@cyrilvincent.com>
2025-10-03 21:57:21 +0200

    recompute iscris

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ version = "1.0.0.RC"
 copyright = "(c) Skema 2024-2025"
 connection_string = "postgresql://postgres:sa@localhost:5432/iscri"
 download_path = "d:/iscri/download2" if os.name == "nt" else "/home/azureuseriscri/download"
-iscri_threshold = 1e-2
+iscri_threshold = 1e-3
 nb_row_commit = 1000000
 
 
